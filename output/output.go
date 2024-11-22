b9cad2aff624186342dfd4d565a6f798b517083d
Jeffrey <kingtingthegreat@gmail.com>
2024-11-21 20:38:14 -0500

    center track text

--- output/output.go
+++ output/output.go
@@ -3,11 +3,23 @@ package output
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/kingtingthegreat/top-fetch-cli/config"
 )
 
+func centerTrackText(trackText string, dim int) string {
+	if len(trackText) > dim {
+		return trackText
+	}
+
+	leftPad := (dim - len(trackText)) / 2
+	rightPad := dim - len(trackText) - leftPad
+	return strings.Repeat(" ", leftPad) + trackText + strings.Repeat(" ", rightPad)
+}
+
 func Output(cfg config.Config, ansiImage, trackText string) {
+	trackText = centerTrackText(trackText, int(cfg.Dim))
 
 	// write to desired output
 	if cfg.File != "" {
